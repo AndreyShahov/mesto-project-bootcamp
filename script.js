90d5eff534cfb834aac88785fbe911a57619f1e8
Andrey Shahov <andreyka.shakhov@mail.ru>
2022-11-26 19:55:13 +0300

    feat: добавить редактирование информации о себе

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const editBtn = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closeBtn = document.querySelector('.popup__close-btn')
+const authorInput = document.querySelector('.popup__text_field_author');
+const sublineInput = document.querySelector('.popup__text_field_author-subline');
+const authorField = document.querySelector('.profile__author');
+const sublineField = document.querySelector('.profile__author-subline');
+const saveBtn = document.querySelector('.popup__save-btn');
 
 
+editBtn.addEventListener('click', () => {
+  popup.classList.add('popup_opened');
 
-editBtn.addEventListener('click', () => popup.classList.add('popup_opened'));
+  authorInput.value = authorField.textContent;
+  sublineInput.value = sublineField.textContent;
+});
+
 closeBtn.addEventListener('click', () => popup.classList.remove('popup_opened'));
+
+ function handleFormSubmit() {
+  const authorValue = authorInput.value;
+  const sublineValue = sublineInput.value;
+
+  authorField.textContent = authorValue;
+  sublineField.textContent = sublineValue;
+
+  popup.classList.remove('popup_opened')
+}
+
+saveBtn.addEventListener('click', handleFormSubmit);
