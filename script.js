36cc9a83aefed5d26c619ff0304ba5807fe3d9c2
unknown <andreyka.shakhov@mail.ru>
2022-11-28 21:07:55 +0300

    feat: add trash

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ const popupEdit = document.querySelector('.popup_type_edit');
 const popupAdd = document.querySelector('.popup_type_add-card');
 const editCloseBtn = popupEdit.querySelector('.popup__close-btn');
 const addCloseBtn = popupAdd.querySelector('.popup__close-btn');
-const authorInput = document.querySelector('.popup__text_field_author');
-const sublineInput = document.querySelector('.popup__text_field_author-subline');
+const editAuthorInput = popupEdit.querySelector('.popup__text_field_author');
+const editSublineInput = popupEdit.querySelector('.popup__text_field_author-subline');
 const authorField = document.querySelector('.profile__author');
 const sublineField = document.querySelector('.profile__author-subline');
 const editSaveBtn = popupEdit.querySelector('.popup__save-btn');
 const addSaveBtn = popupAdd.querySelector('.popup__save-btn');
 const elements = document.querySelector('.elements');
+const elementImage = element.querySelector('.element__image');
+const addAuthorInput = popupAdd.querySelector('.popup__text_field_author');
+const addSublineInput = popupAdd.querySelector('.popup__text_field_author-subline');
+const likeBtn = document.querySelector('.element__like-btn');
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
@@ -26,23 +30,9 @@ function closePopup(popup) {
 editCloseBtn.addEventListener('click', () => closePopup(popupEdit));
 addCloseBtn.addEventListener('click', () => closePopup(popupAdd));
 
-function createCard() {
-  // const elementTemplate = document.querySelector('#element').content;
-  let newElement = element.content.cloneNode(true);
-
-  newElement.querySelector('element__image').setAttribute('src', 'https://images.unsplash.com/photo-1669492223198-0664d4c9d2ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1202&q=80')
-  newElement.querySelector('element__title').textContent = authorInput.value;
-  elements.prepend(newElement);
-
-  popupAdd.classList.remove('popup_opened');
-}
-
-addSaveBtn.addEventListener('click', createCard);
-
-
 function handleFormSubmit() {
-  const authorValue = authorInput.value;
-  const sublineValue = sublineInput.value;
+  const authorValue = editAuthorInput.value;
+  const sublineValue = editSublineInput.value;
 
   authorField.textContent = authorValue;
   sublineField.textContent = sublineValue;
@@ -52,12 +42,27 @@ function handleFormSubmit() {
 
 editSaveBtn.addEventListener('click', handleFormSubmit);
 
-// addBtn.addEventListener('click', () => {
-//   const popupCopy = popup.cloneNode(true);
+function addCard() {
+  const elementTemplate = document.querySelector('#element').content;
+  const newElement = elementTemplate.cloneNode(true);
 
-//   popupCopy.classList.add('popup_opened');
+  newElement.querySelector('.element__image').src = addSublineInput.value;
+  newElement.querySelector('.element__title').textContent = addAuthorInput.value;
 
-//   const popupHeading = querySelector('.popup__heading');
-//   popupHeading.textContent = 'Новое место';
-// });
+  elements.prepend(newElement);
 
+  popupAdd.classList.remove('popup_opened');
+  popupAdd.querySelector('form').reset();
+}
+
+addSaveBtn.addEventListener('click', addCard);
+
+function like() {
+  likeBtn.classList.toggle('element__like-btn_active');
+}
+
+likeBtn.addEventListener('click', like);
+
+function deletePopup() {
+  add.classList.remove('');
+}
