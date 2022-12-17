fc23dbea653184cc9a4d774e6deef31522890882
Andrey Shahov <andreyka.shakhov@mail.ru>
2022-12-17 10:13:00 +0300

    feat: редактирование автара - overlay

--- src/components/card.js
+++ src/components/card.js
@@ -22,45 +22,72 @@ function createCard(item) {
       }
     });
   });
-
+  // fetch('https://nomoreparties.co/v1/wbf-cohort-3/cards', {
+  //   headers: {
+  //     authorization: '760e0d80-494a-4d91-971a-4eb297900ae7'
+  //   }
+  // })
+  // .then(res => {
+  //   if (res.ok) {
+  //     return res.json();
+  //   } else {
+  //     return Promise.reject(`Что-то не так: ${res.status}`);
+  //   }
+  // })
+  // .then(items => {
+  //   items.forEach(item => {
+  //     if (item['likes'].find(item => item['_id'] === "135e568bbf5b7f0594e3ab64")) {
+  //       console.log('да');
+  //       console.log(item)
+  //       likeBtn.classList.add('element__like-btn_active');
+  //     }
+  //   })
+  // })
+  // .catch(err => console.log(err));
   likeBtn.addEventListener('click', () => {
-    likeBtn.classList.toggle('element__like-btn_active');
-    if (likeBtn.classList.contains('element__like-btn_active')) {
-      fetch(`https://nomoreparties.co/v1/wbf-cohort-3/cards/likes/${item['_id']}`, {
-        method: 'PUT',
-        headers: {
-          authorization: '760e0d80-494a-4d91-971a-4eb297900ae7'
-        }
-      })
-      .then((res) => {
-        if(res.ok) {
-          return res.json();
-        } else {
-        return Promise.reject(`Что-то не так: ${res.status}`);
-        }
-      })
-      .then((item) => {
-        counterLikes.textContent = item.likes.length;
-      })
-    } else {
-      fetch(`https://nomoreparties.co/v1/wbf-cohort-3/cards/likes/${item['_id']}`, {
-        method: 'DELETE',
-        headers: {
-          authorization: '760e0d80-494a-4d91-971a-4eb297900ae7'
-        }
-      })
-      .then((res) => {
-        if(res.ok) {
-          return res.json();
-        } else {
-        return Promise.reject(`Что-то не так: ${res.status}`);
-        }
-      })
-      .then((item) => {
-        counterLikes.textContent = item.likes.length;
-      })
-    }
+
+    // likeBtn.classList.toggle('element__like-btn_active');
+
+
+
+    // if (likeBtn.classList.contains('element__like-btn_active')) {
+    //   fetch(`https://nomoreparties.co/v1/wbf-cohort-3/cards/likes/${item['_id']}`, {
+    //     method: 'PUT',
+    //     headers: {
+    //       authorization: '760e0d80-494a-4d91-971a-4eb297900ae7'
+    //     }
+    //   })
+    //   .then((res) => {
+    //     if(res.ok) {
+    //       return res.json();
+    //     } else {
+    //     return Promise.reject(`Что-то не так: ${res.status}`);
+    //     }
+    //   })
+    //   .then((item) => {
+    //     counterLikes.textContent = item.likes.length;
+    //   })
+    // } else {
+    //   fetch(`https://nomoreparties.co/v1/wbf-cohort-3/cards/likes/${item['_id']}`, {
+    //     method: 'DELETE',
+    //     headers: {
+    //       authorization: '760e0d80-494a-4d91-971a-4eb297900ae7'
+    //     }
+    //   })
+    //   .then((res) => {
+    //     if(res.ok) {
+    //       return res.json();
+    //     } else {
+    //     return Promise.reject(`Что-то не так: ${res.status}`);
+    //     }
+    //   })
+    //   .then((item) => {
+    //     counterLikes.textContent = item.likes.length;
+    //   })
+    // }
   })
+
+
 
   counterLikes.textContent = item.likes.length;
 
@@ -92,11 +119,7 @@ function addStrangeCard(item, container) {
 }
 
 
- fetch('https://nomoreparties.co/v1/wbf-cohort-3/cards', {
-  headers: {
-    authorization: '760e0d80-494a-4d91-971a-4eb297900ae7'
-  }
-})
+getCards()
   .then(res => {
     if (res.ok) {
       return res.json();
@@ -112,12 +135,21 @@ function addStrangeCard(item, container) {
       } else {
         addStrangeCard(item, elements);
       }
-
-
     })
   })
   .catch(err => console.log(err));
 
 export { addNewCard };
+
+function getCards() {
+  return fetch('https://nomoreparties.co/v1/wbf-cohort-3/cards', {
+    headers: {
+      authorization: '760e0d80-494a-4d91-971a-4eb297900ae7'
+    }
+  });
+}
+
+document.addEventListener('click', (evt) => console.log(evt.target));
+
 
 
